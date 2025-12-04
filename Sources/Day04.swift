4a2ea802261cf21164e64c673bf13a082b645bda
Rome Abuziarov <roman.abuziarov@reddit.com>
2025-12-04 11:33:32 +0100

    Day 04

---
+++ Sources/Day04.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+struct Day04: AdventDay {
+  private let data: [[Bool]]
+
+  init(data: String) {
+    self.data = data.split(separator: "\n").map { line in
+      line.map { $0 == "@" }
+    }
+  }
+
+  func part1() -> Int {
+    var solver = Solver(data: data)
+    return solver.countAccessibleRolls()
+  }
+
+  func part2() -> Int {
+    var solver = Solver(data: data)
+    var total = 0
+    var last = -1
+    while last != 0 {
+      let accessible = solver.countAccessibleRolls(prune: true)
+      last = accessible
+      total += last
+    }
+    return total
+  }
+}
+
+private struct Solver {
+  private var data: [[Bool]]
+
+  init(data: [[Bool]]) {
+    self.data = data
+  }
+
+  mutating func countAccessibleRolls(prune: Bool = false) -> Int {
+    var accessible = [(row: Int, col: Int)]()
+    for row in data.indices {
+      for col in data[row].indices {
+        guard data[row][col] else { continue }
+        var rolls = 0
+        iterateNeighbors(row, col) { row, col in
+          if data[row][col] {
+            rolls += 1
+          }
+          return rolls < 4
+        }
+        if rolls < 4 {
+          accessible.append((row, col))
+        }
+      }
+    }
+    if prune {
+      for (row, col) in accessible {
+        data[row][col] = false
+      }
+    }
+    return accessible.count
+  }
+
+  private func iterateNeighbors(
+    _ row: Int,
+    _ col: Int,
+    proceed: (_ row: Int, _ col: Int) -> Bool
+  ) {
+    for i in [row - 1, row, row + 1] {
+      for j in [col - 1, col, col + 1] {
+        guard i != row || j != col,
+          data.indices.contains(i),
+          data[i].indices.contains(j)
+        else { continue }
+        if !proceed(i, j) {
+          return
+        }
+      }
+    }
+  }
+}
---
+++ Tests/Day04.swift
@@ -0,0 +1,28 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day04Tests {
+  let testData = """
+    ..@@.@@@@.
+    @@@.@.@.@@
+    @@@@@.@.@@
+    @.@@@@..@.
+    @@.@@@@.@@
+    .@@@@@@@.@
+    .@.@.@.@@@
+    @.@@@.@@@@
+    .@@@@@@@@.
+    @.@.@@@.@.
+    """
+
+  @Test func testPart1() async throws {
+    let challenge = Day04(data: testData)
+    #expect(String(describing: challenge.part1()) == "13")
+  }
+
+  @Test func testPart2() async throws {
+    let challenge = Day04(data: testData)
+    #expect(String(describing: challenge.part2()) == "43")
+  }
+}
